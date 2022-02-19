c6f43d12bebdc94c4363ed6293cd6ff7491ba33e
EzequielMessore <ezequiel.messore@hotmail.com>
2022-02-18 23:35:59 -0300

    - Removing imports unnecessary

--- app/src/main/kotlin/br/com/messore/tech/turtleracing/ui/MainActivity.kt
+++ app/src/main/kotlin/br/com/messore/tech/turtleracing/ui/MainActivity.kt
@@ -1,23 +1,16 @@
 package br.com.messore.tech.turtleracing.ui
 
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.work.OneTimeWorkRequestBuilder
-import androidx.work.WorkManager
 import br.com.messore.tech.turtleracing.presentation.login.LoginViewModel
 import br.com.messore.tech.turtleracing.ui.theme.TurtleRacingTheme
 import br.com.messore.tech.turtleracing.workers.RunnerWorker
